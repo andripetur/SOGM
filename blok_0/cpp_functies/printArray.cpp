284c26cc8f90e7654d3626ba20bdc69314c5409f
Andri Pétur Þrastarson <andripeterstummi@gmail.com>
2014-10-09 22:15:42 +0200

    Make file added, gitignore and printArray updated.

--- blok_0/cpp_functies/printArray.cpp
+++ blok_0/cpp_functies/printArray.cpp
@@ -1,10 +1,4 @@
 #include <iostream>
-#include <sstream>
-#include <iterator>
-#include <vector>
-#include <string>
-
-
 
 using namespace std;
 
@@ -16,50 +10,6 @@ void printArray(string array[], int length) {
 }
 
 
-void stringToArray(string sInput,string daArray[], int length){
-
-	int i = 0;
-	stringstream ssin(sInput);
-	while (ssin.good() && i < length){
-		ssin >> daArray[i];
-		++i;
-	}
-}
-
-
-/*
-void stringToArray(char* str,string array[], int length){
-
-	const char* ptr = str;
-	for(int i = 0; i < length; i++){
-   	array[i] = ptr;
-  	 ptr += strlen(ptr)+1;
-	}
-}
-*/
-	
-
-int main() {
-	int length;
-	string input; 
-	
-	cout << "The length of the array: ";
-	cin >> length;
-
-	string *array = new string[length];
-
-	cout << "The array: ";
-	cin >> input;
-
-	stringToArray(input, array, length);
-
-	printArray(array,length);
-
-	return 0;
-}
-
-
-/*
 int main() {
 	int length=5;
 	string array[] ={"This", "is", "an", "array", "."};
@@ -69,5 +19,5 @@ int main() {
 	return 0;
 }
 
-*/	
+
 	
